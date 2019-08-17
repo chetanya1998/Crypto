549857b738526946e6fcb72caa8df33454a56333
chetanya1998 <chetanyaved@gmail.com>
2019-08-17 18:24:04 +0530

    Add App Layout

---
+++ src/App/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import WelcomeMessage from'./WelcomeMessage'
+
+import './App.css';
+import styled,{css} from'styled-components';
+import AppLayout from './AppLayout';
+
+// const MyButton = styled.div`
+// color:green;
+// ${props => props.primary && css`
+//
+//     color: palevioletred;
+//   `}
+// `
+// const TomatoButton = styled(MyButton)`
+//   color: tomato;
+//   border-color: tomato;
+// `;
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <AppLayout>
+    <WelcomeMessage />
+
+
+
+    </AppLayout>
+    </div>
+  );
+}
+
+export default App;
